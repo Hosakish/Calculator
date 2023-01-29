6fa9e9bc5f916db8f61696c3fbf07235db1e0bf5
hosakish <123302812+Hosakish@users.noreply.github.com>
2023-01-29 19:21:57 +0100

    equal

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-const btns = document.getElementsByClassName("calc-btn")
-
-for(let i = 0; i<btns.length;i++){
-    btns[i].addEventListener("click", ()=>[
-        console.log("index", i)
-    ])
+const btns = document.getElementsByClassName("calc-btn");
+const answer = document.getElementById("answer");
+const equal = document.getElementById("equal");
+const del = document.getElementById("delete");
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", (e) => {
+    const number = e.target.innerText;
+    if (answer.innerText.toLowerCase() === "sum") {
+      answer.innerText = "";
+    }
+    answer.innerText += number;
+  });
 }
+del.addEventListener("click", () => {
+  answer.innerText = "Sum";
+});
+equal.addEventListener("click", () => {
+  const equation = answer.innerText;
+  if (equation.includes("+")) {
+    const equationSplit = equation.split("+");
+    answer.innerText = Number(equationSplit[0]) + Number(equationSplit[1]);
+  }
+});
